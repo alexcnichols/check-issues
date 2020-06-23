56d60d13d70723a38da1472b4d67bba1b26c67f3
Alex Nichols <alexcnichols@gmail.com>
2020-06-23 01:00:56 -0700

    Fix comment format

--- get-message.test.js
+++ get-message.test.js
@@ -1,11 +1,10 @@
 const getMessage = require('./get-message');
 
 test('gets message with actor', async() => {
-    await expect(getMessage('alexcnichols')).toBe(
-        `
-    Hello @alexcnichols :wave: - Thanks for opening a release issue. After checking the issue against the guidelines, here are some next steps:
-    
-    - Add this issue to the **Release tracker** project board, which is organized with columns representing months of the year. Select the column corresponding to this release's next ship date.
-    `
+    await expect(getMessage('alexcnichols')).toBe(`
+Hello @alexcnichols :wave: - Thanks for opening a release issue. After checking the issue against the guidelines, here are some next steps:
+
+- Add this issue to the **Release tracker** project board, which is organized with columns representing months of the year. Select the column corresponding to this release's next ship date.
+`
     )
 });
